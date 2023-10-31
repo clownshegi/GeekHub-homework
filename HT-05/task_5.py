95a7874d4a92ade093596e4c5c1ca8c547337034
clownshegi <avidmenko3@gmail.com>
2023-10-31 20:37:01 +0200

    fix hometask_5

--- HT-05/task_5.py
+++ HT-05/task_5.py
@@ -1,25 +1,19 @@
-# Ну і традиційно - калькулятор :slightly_smiling_face: Повинна бути 1 ф-цiя, яка б приймала 3 аргументи - один з яких операцiя, яку зробити! Аргументи брати від юзера (можна по одному - 2, окремо +, окремо 2; можна всі разом - типу 1 + 2). Операції що мають бути присутні: +, -, *, /, %, //, **. Не забудьте протестувати з різними значеннями на предмет помилок!
-
-def calculator(operator, operand1, operand2):
+def calculator(operator, operand_1, operand_2):
     try:
         if operator == '+':
-            result = operand1 + operand2
+            result = operand_1 + operand_2
         elif operator == '-':
-            result = operand1 - operand2
+            result = operand_1 - operand_2
         elif operator == '*':
-            result = operand1 * operand2
+            result = operand_1 * operand_2
         elif operator == '/':
-            if operand2 == 0:
-                return "Деление на ноль невозможно"
-            result = operand1 / operand2
+            result = operand_1 / operand_2
         elif operator == '%':
-            result = operand1 % operand2
+            result = operand_1 % operand_2
         elif operator == '//':
-            if operand2 == 0:
-                return "Деление на ноль невозможно"
-            result = operand1 // operand2
+            result = operand_1 // operand_2
         elif operator == '**':
-            result = operand1 ** operand2
+            result = operand_1 ** operand_2
         else:
             return "Неизвестный оператор"
         return f"Результат: {result}"
@@ -28,9 +22,10 @@ def calculator(operator, operand1, operand2):
     except Exception as e:
         return f"Ошибка: {e}"
 
-operator = input("Введите оператор (+, -, *, /, %, //, **): ")
-operand1 = float(input("Введите первый операнд: "))
-operand2 = float(input("Введите второй операнд: "))
 
-result = calculator(operator, operand1, operand2)
+operator = input("Введите оператор (+, -, *, /, %, //, **): ")
+operand_1 = float(input("Введите первый операнд: "))
+operand_2 = float(input("Введите второй операнд: "))
+
+result = calculator(operator, operand_1, operand_2)
 print(result)
