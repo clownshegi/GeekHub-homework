5bd42a3bb856b20c18b820a373af56478a9663a4
clownshegi <avidmenko3@gmail.com>
2023-12-02 04:04:19 +0200

    hometask_14

---
+++ HT-14/task_2.py
@@ -0,0 +1,81 @@
+"""
+Створіть програму для отримання курсу валют за певний період.
+- отримати від користувача дату (це може бути як один день так і інтервал - початкова
+і кінцева дати, продумайте механізм реалізації) і назву валюти
+- вивести курс по відношенню до гривні на момент вказаної дати (або за кожен день у вказаному інтервалі)
+- не забудьте перевірку на валідність введених даних
+"""
+import datetime
+import requests
+
+CURRENCY_CODES = ['USD', 'EUR']
+
+
+def get_dates():
+    print("Виберіть варіант:")
+    print("1 - конкретна дата")
+    print("2 - інтервал дат\n")
+
+    choice = input("Ваш вибір (1/2)? ")
+
+    if choice == "1":
+        date_text = input("Введіть дату (РРРР-ММ-ДД): ")
+        try:
+            date = datetime.datetime.strptime(date_text, "%Y-%m-%d")
+            return [date]
+        except ValueError:
+            print("Невірний формат дати!\n")
+            return get_dates()
+    elif choice == "2":
+        try:
+            start_text = input("Початкова дата (РРРР-ММ-ДД): ")
+            start_date = datetime.datetime.strptime(start_text, "%Y-%m-%d")
+
+            end_text = input("Кінцева дата (РРРР-ММ-ДД): ")
+            end_date = datetime.datetime.strptime(end_text, "%Y-%m-%d")
+
+            if start_date > end_date:
+                print("Початкова дата повинна бути меншою за кінцеву!")
+                return get_dates()
+
+            dates_range = []
+            current_date = start_date
+            while current_date <= end_date:
+                dates_range.append(current_date)
+                current_date += datetime.timedelta(days=1)
+
+            return dates_range
+        except ValueError:
+            print("Невірний формат дати!\n")
+            return get_dates()
+    else:
+        print("\nНевірний вибір! Виберіть 1 або 2.")
+        return get_dates()
+
+
+def get_currency():
+    currency = input("Введіть код валюти(USD \ EUR): ").upper()
+    if currency not in CURRENCY_CODES:
+        print("Невірний код валюти!")
+        return get_currency()
+    else:
+        return currency
+
+
+def get_rates(date, currency):
+    url = f'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?date={date.strftime("%Y%m%d")}&json'
+    resp = requests.get(url)
+    resp.raise_for_status()
+
+    rates = resp.json()
+    for rate in rates:
+        if rate['cc'] == currency:
+            return rate['rate']
+
+
+dates = get_dates()
+currency = get_currency()
+
+for date in dates:
+    rate = get_rates(date, currency)
+    print(f'{currency} до гривні на {date:%d.%m.%Y}: {rate}')
---
+++ HT-14/task_3.py
@@ -0,0 +1,57 @@
+"""
+3. http://quotes.toscrape.com/ - написати скрейпер для збору всієї доступної інформації про записи:
+ цитата, автор, інфа про автора тощо.
+- збирається інформація з 10 сторінок сайту.
+- зберігати зібрані дані у CSV файл
+"""
+import requests
+from bs4 import BeautifulSoup
+from urllib.parse import urljoin
+import csv
+
+url = 'http://quotes.toscrape.com/page/{}/'
+base_url = 'http://quotes.toscrape.com'
+
+quotes = []
+
+for page in range(1, 11):
+    print(f'Парсинг сторінки {page}')
+    res = requests.get(url.format(page))
+    soup = BeautifulSoup(res.text, 'html.parser')
+
+    for quote in soup.find_all(class_='quote'):
+        text = quote.find(class_='text').get_text(strip=True)
+        author = quote.find(class_='author').get_text(strip=True)
+
+        bio_link = quote.find('a')['href']
+        bio_link = urljoin(base_url, bio_link)
+
+        bio_res = requests.get(bio_link)
+        bio_soup = BeautifulSoup(bio_res.text, 'html.parser')
+
+        born_date = bio_soup.find(class_='author-born-date').get_text(strip=True)
+        born_location = bio_soup.find(class_='author-born-location').get_text(strip=True)
+        bio = bio_soup.find(class_='author-description').get_text(strip=True)
+
+        quotes.append({
+            'text': text,
+            'author': author,
+            'born_date': born_date,
+            'born_location': born_location,
+            'bio': bio
+        })
+
+with open('quotes_ua.csv', 'w', encoding='utf-16') as file:
+    writer = csv.writer(file)
+
+    for quote in quotes:
+        writer.writerow([])
+        writer.writerow([quote['text']])
+        writer.writerow([quote['author']])
+        writer.writerow([quote['born_date']])
+        writer.writerow([quote['born_location']])
+        writer.writerow([quote['bio']])
+
+print('parsing finished')
+
+print('Парсинг закінчено, цитати збережено у quotes_ua.csv')
