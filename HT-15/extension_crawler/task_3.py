f4e465fbb18416a01a2c0d278e808f7162e94a00
clownshegi <avidmenko3@gmail.com>
2023-12-09 07:03:10 +0200

    hometask_15

--- HT-15/extension_crawler/task_3.py
+++ HT-15/extension_crawler/task_3.py
@@ -1,4 +1,10 @@
+"""
+Використовуючи Scrapy, заходите на "https://chrome.google.com/webstore/sitemap", переходите на кожен лінк з тегів <loc>,
+ з кожного лінка берете посилання на сторінки екстеншенів, парсите їх і зберігаєте в CSV файл ID, назву та короткий
+ опис кожного екстеншена (пошукайте уважно де його можна взяти)
+"""
+
 import subprocess
 
 if __name__ == "__main__":
-    subprocess.run(["scrapy", "crawl", "google", "-O", "data.csv"])
+    subprocess.run(["scrapy", "crawl", "google", "-O", "info.csv"])
