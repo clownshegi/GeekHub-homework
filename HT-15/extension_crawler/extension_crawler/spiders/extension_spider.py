15d51950d21fbb84a2d238b6a72fd0ab8879d4f0
clownshegi <avidmenko3@gmail.com>
2023-12-09 06:57:25 +0200

    hometask_15

--- HT-15/extension_crawler/extension_crawler/spiders/extension_spider.py
+++ HT-15/extension_crawler/extension_crawler/spiders/extension_spider.py
@@ -4,24 +4,24 @@ from bs4 import BeautifulSoup
 
 class GoogleSpider(scrapy.Spider):
     name = 'google'
-    BASE_URL = 'https://chrome.google.com/webstore/sitemap'
+    base_url = 'https://chrome.google.com/webstore/sitemap'
 
     def start_requests(self):
-        yield scrapy.Request(url=self.BASE_URL, callback=self.parse_map)
+        yield scrapy.Request(url=self.base_url, callback=self.parse_map)
 
     def parse_map(self, response):
-        soup = BeautifulSoup(response.body, 'xml')
+        soup = BeautifulSoup(response.body, 'html.parser')
         for url in soup.select('sitemap > loc'):
             yield scrapy.Request(url=url.text, callback=self.parse_urls)
 
     def parse_urls(self, response):
-        soup = BeautifulSoup(response.body, 'xml')
+        soup = BeautifulSoup(response.body, 'html.parser')
         for url in soup.select('url > loc'):
             if "/detail" not in url.text:
                 continue
             yield scrapy.Request(url=url.text, callback=self.parse_extensions)
 
-    def parse_extensions(self, response, **kwargs):
+    def parse_extensions(self, response):
         id_ = response.css('[property="og:url"]::attr(content)').get().split('/').pop()
         yield {
             'id': f'{id_}',
